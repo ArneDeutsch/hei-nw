b30d546e1bc077ae5f5487a5387af7394fd69c39
Arne Deutsch <Arne.Deutsch@itemis.de>
2025-09-12 12:10:44 +0200

    test: cover episodic store

--- src/hei_nw/__init__.py
+++ src/hei_nw/__init__.py
@@ -1,5 +1,14 @@
 """HEI NW package."""
 
-__all__ = ["__version__"]
+from .keyer import DGKeyer
+from .store import ANNIndex, EpisodicStore, HopfieldReadout
+
+__all__ = [
+    "__version__",
+    "DGKeyer",
+    "ANNIndex",
+    "HopfieldReadout",
+    "EpisodicStore",
+]
 
 __version__ = "0.0.0"
